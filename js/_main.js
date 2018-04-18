002aecedca555a32a7832fa965bf50eee9fdc244
Fernando Rivas <fernando.rivas@ponderalab.com>
2018-04-17 20:47:20 -0500

    Trying to particles. Again paths

--- js/_main.js
+++ js/_main.js
@@ -17,9 +17,7 @@ $(document).ready(function(){
 });
 
 // particlesJS.load(@dom-id, @path-json, @callback (optional));
-particlesJS.load('particles-js', '_data/particles.json', function() {
-      console.log('callback - particles.js config loaded');
-});
+particlesJS.load("particles-js", "{{ site.url }}/_data/particles.json");
 
 // Table of Contents title. Change text to localize
 $("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
